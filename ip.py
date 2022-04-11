bf54bfafe453a05bc1286ed78340a802b77d249d
theosassan <103321888+theosassan@users.noreply.github.com>
2022-04-11 17:17:37 -0400

    Delete ip.py

--- ip.py
+++
@@ -1,9 +0,0 @@
-import requests
-import os
-def ip():
-  proxies = {
-  "http": os.environ['QUOTAGUARDSTATIC_URL'],
-  "https": os.environ['QUOTAGUARDSTATIC_URL']
-  }
-
-  res = requests.get("http://ip.quotaguard.com/", proxies=proxies)
