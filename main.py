aa291200e0ceb5c3b5163017fa4e5c2199b50b68
theosassan <103321888+theosassan@users.noreply.github.com>
2022-04-11 19:43:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2076,7 +2076,7 @@ proxies = {
     "http": os.environ['QUOTAGUARDSTATIC_URL'],
     "https": os.environ['QUOTAGUARDSTATIC_URL']
     }
-    res = requests.get("http://us-east-static-08.quotaguard.com/", proxies=proxies)
+res = requests.get("http://us-east-static-08.quotaguard.com/", proxies=proxies)
     #res = requests.get("http://us-east-static-08.quotaguard.com/", "http://lbzz6x1r4y1xf:yujtw4nnp7bs38xpzmw9lk0atw@us-east-static-08.quotaguard.com:9293")
     REQUEST_LOG = '{method} {url} recieved {text} has returned {status}'
 
